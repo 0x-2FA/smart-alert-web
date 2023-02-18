3df3d78bd50245803c4a0344cf1b44b3d456738f
SophiaNikolia <sofia.nikolia@gmail.com>
2023-02-18 21:05:28 +0200

    Initial commit

---
+++ Controllers/AlertsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace smart_alert_web.Controllers
+{
+    public class AlertsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAlert()
+        {
+            return View();
+        }
+        public IActionResult SendAlert()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace smart_alert_web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
